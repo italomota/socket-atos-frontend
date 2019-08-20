54b22949792ff7110d900226156dae0b71b4b0ba
Italo Mota <italo.mota@atoscapital.com.br>
2019-08-20 18:44:25 -0300

    Projeto socket-atos frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      io: socketIOClient('http://192.168.0.25:3333'),
+      mensagens: [],
+      novaMensagem: '',
+      sala: false,
+    }
+
+    this.ouvirMensagens()
+  }
+
+  enviarMensagem = () => {
+    const { novaMensagem, io } = this.state
+    
+    this.setState({ mensagens: [...this.state.mensagens, novaMensagem] })
+
+    io.emit('novaMensagem', novaMensagem)
+
+    this.setState({ novaMensagem: '' })
+  }
+
+  enviarMensagemSala = () => {
+    const { novaMensagem, io } = this.state
+
+    io.emit('novaMensagemSala', novaMensagem)
+
+    this.setState({ novaMensagem: '' })
+  }
+
+  ouvirMensagens = () => {
+    const { io } = this.state
+
+    io.on('mensagens', novaMensagem => {
+      this.setState({ mensagens: [...this.state.mensagens, novaMensagem] })
+    })
+
+    io.on('sala', () => {
+      this.setState({ sala: !this.state.sala })
+    })
+  }
+
+  render() {
+    const { mensagens, novaMensagem, io, sala } = this.state
+
+    return (
+      <>
+        {!sala && (
+          <button onClick={() => io.emit('entrarSala', '1107')}>
+            Entrar na sala
+          </button>
+        )}
+
+        {sala && (
+          <button onClick={() => io.emit('sairSala', '1107')}>
+            Sair da sala
+          </button>
+        )}
+
+        <ul>
+          {mensagens.map((mensagem, index) => (
+            <li key={index}>{mensagem}</li>
+          ))}
+        </ul>
+
+        <input
+          autoFocus
+          value={novaMensagem}
+          onChange={e => this.setState({ novaMensagem: e.target.value })}
+        />
+
+        <br /><br />
+
+        <button onClick={this.enviarMensagem}>
+          Enviar
+        </button>
+
+        <button onClick={this.enviarMensagemSala}>
+          Enviar para sala
+        </button>
+        
+      </>
+    )
+  }
 }
-
-export default App;
